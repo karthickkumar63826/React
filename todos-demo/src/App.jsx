59ea1f425a0bb5df7c1c939f9a9bc73f92030252
karthickkumar63836 <karthickkumar63826@gmail.com>
2024-02-13 13:54:13 +0530

    updated

--- todos-demo/src/App.jsx
+++ todos-demo/src/App.jsx
@@ -3,23 +3,39 @@ import "./App.css";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { FaRegTrashCan } from "react-icons/fa6";
+import { FaEdit } from "react-icons/fa";
 
 function App() {
   let [todos, setTodos] = useState([]); // array of todos
   let [todoInput, setTodoInput] = useState(""); // input
+  let [editTodo, setEditTodo] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault(); //to prevent form refresh/reload
+    if (editTodo.id) {
+      const updateTodos = todos.map((todo) => {
+        if (todo.id === editTodo.id) {
+          return { ...todo, text: todoInput };
+        } else {
+          return todo;
+        }
+      });
 
-    const addTool = {
-      id: uuidv4(),
-      text: todoInput,
-      complete: false,
-    };
+      setTodos(updateTodos);
+      console.log(todos);
+      setEditTodo({});
+    } else {
+      const addTool = {
+        id: uuidv4(),
+        text: todoInput,
+        completed: false,
+      };
 
-    todos.push(addTool);
-    setTodos(todos);
-    console.log(todos);
+      todos.push(addTool);
+      setTodos(todos);
+      console.log(todos);
+    }
+
     setTodoInput("");
   };
 
@@ -28,6 +44,23 @@ function App() {
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     console.log(updatedTodos);
     setTodos(updatedTodos);
+  };
+
+  const updatedTodo = (todo) => {
+    setTodoInput(todo.text);
+    setEditTodo(todo);
+  };
+
+  const updatecompleted = (completedTodo) => {
+    const updateTodo = todos.map((todo) => {
+      if (completedTodo.id === todo.id) {
+        return { ...todo, completed: !todo.completed };
+      } else {
+        return todo;
+      }
+    });
+
+    setTodos(updateTodo);
   };
   return (
     <div className="App">
@@ -47,7 +80,7 @@ function App() {
       <br />
 
       {todos.length < 1 ? (
-        "You have no task to complete"
+        "You have no task to completed"
       ) : (
         <b>You have {todos.length} task to complete</b>
       )}
@@ -55,9 +88,13 @@ function App() {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id} style={{ listStyle: "none" }}>
-            {todo.text}{" "}
+            <input type="checkbox" onChange={() => updatecompleted(todo)} />
+            {todo.completed ? <del>{todo.text}</del> : todo.text}{" "}
             <button onClick={() => deleteTodo(todo.id)}>
               <FaRegTrashCan />
+            </button>
+            <button onClick={() => updatedTodo(todo)}>
+              <FaEdit />
             </button>
           </li>
         ))}
