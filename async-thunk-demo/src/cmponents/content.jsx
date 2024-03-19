16ce937b601c10dbe4fdb3c85d11b95e58a24eba
karthickkumar63836 <karthickkumar63826@gmail.com>
2024-03-19 18:12:11 +0530

    updated

--- async-thunk-demo/src/cmponents/content.jsx
+++ async-thunk-demo/src/cmponents/content.jsx
@@ -1,27 +1,20 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { fetchContent } from "../reducers/contentSlice";
 
-export default function Content(){
+export default function Content() {
+  let data = useSelector((state) => state.contentStore);
 
-    let data = useSelector((state) => state.contentStore);
+  let dispatch = useDispatch();
 
-    let dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContent());
+  });
 
-    useEffect(()=>{
-        dispatch(fetchContent());
-    })
-
-    return(
-        <>
-        <h1>Content</h1>
-        {
-            data.content.map((c) => return(
-                <h1>{c.title}</h1>
-                <p>{c.Body}</p>
-                <hr />
-            ))
-        }
-        </>
-    )
+  return (
+    <>
+      <h1>Content</h1>
+      {JSON.stringify(data)}
+    </>
+  );
 }
